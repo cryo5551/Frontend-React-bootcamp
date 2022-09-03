7665ce2f7f219a576aed28ccfde08aaea7c9effc
Bhawani Shanakar <jbsmj555@gmail.com>
2022-09-03 23:16:33 +0530

    bootstrap

--- React/2022-09-03/project_bootstrap/src/App.js
+++ React/2022-09-03/project_bootstrap/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Nevbar from './components/Nevbar';
+import 'D:/Frontend React bootcamp/node_modules/bootstrap/dist/css/bootstrap.css';
+import 'D:/Frontend React bootcamp/node_modules/bootstrap/dist/js/bootstrap.js';
+import Carousel from './components/Carousel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nevbar/>
+      <Carousel/>
     </div>
   );
 }
