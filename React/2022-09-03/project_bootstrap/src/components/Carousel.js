d8a77045467d84ff6913ecf9bbb9da9ef918fbc5
Bhawani Shanakar <jbsmj555@gmail.com>
2022-09-19 12:13:11 +0530

    3 sept

--- React/2022-09-03/project_bootstrap/src/components/Carousel.js
+++ React/2022-09-03/project_bootstrap/src/components/Carousel.js
@@ -1,48 +1,50 @@
-import React from 'react';
+// import React from 'react';
 import dp from '../assets/lol1.jpg';
 import pic from '../assets/lol3.png';
 
-function Carousel() {
+import Carousel from 'react-bootstrap/Carousel';
+
+function IndividualIntervalsExample() {
   return (
-    <div><div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src={dp} class="d-block w-100" alt="..."/>
-        {/* <div class="carousel-caption d-none d-md-block">
-          <h5>First slide label</h5>
-          <p>Some representative placeholder content for the first slide.</p>
-        </div> */}
-      </div>
-      <div class="carousel-item">
-        <img src={pic} class="d-block w-100" alt="..."/>
-        {/* <div class="carousel-caption d-none d-md-block">
-          <h5>Second slide label</h5>
-          <p>Some representative placeholder content for the second slide.</p>
-        </div> */}
-      </div>
-      <div class="carousel-item">
-        <img src={dp} class="d-block w-100" alt="..."/>
-        {/* <div class="carousel-caption d-none d-md-block">
-          <h5>Third slide label</h5>
-          <p>Some representative placeholder content for the third slide.</p>
-        </div> */}
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div></div>
-  )
+    <Carousel>
+      <Carousel.Item interval={4000}>
+        <img
+          className="d-block w-100"
+          src={dp}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={500}>
+        <img
+          className="d-block w-100"
+          src={pic}
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={dp}
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
 
-export default Carousel
+
+export default IndividualIntervalsExample;
