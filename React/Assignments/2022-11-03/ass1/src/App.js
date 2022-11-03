cdbe986de8b47e5ad9b52d9a8ab272c9b520921e
Bhawani Shanakar <jbsmj555@gmail.com>
2022-11-03 10:45:39 +0530

    assignment 1

---
+++ React/Assignments/2022-11-03/ass1/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import About from './components/About';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <About/>
+    </div>
+  );
+}
+
+export default App;
